d2209ee63da4d698e96a310269a5a6fb951eb4e2
ewhitebloom <ewhitebloom@gmail.com>
2014-04-02 14:35:48 -0400

    add user_creates_an_account_spec

---
+++ spec/features/user_creates_an_account_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+feature "User adds entry to registry" do
+# As a user,
+# I want to securely register for an account,
+# so that I can keep track of my activity.
+
+#  Acceptance Criteria
+# *A sign up and authentication system for your users (with Devise)
+# *I must provide  an unused email
+# *I must provide a password
+# *Provide an option for first name and last name
+
+  it "can sign up" do
+    count = User.count
+    visit new_user_registration_path
+    fill_in 'Email', with: 'hi@gmail.com'
+    fill_in 'Password', with: 'princely'
+    fill_in 'Password Confirmation', with: 'princely'
+    fill_in 'Username', with: 'Someone'
+    click_on 'Sign Up'
+    expect(page).to have_content "Welcome"
+    expect(User.count).to eql(count + 1)
+  end
+end
