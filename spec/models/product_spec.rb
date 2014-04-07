1dcbf5465b6d955eab2c2723270cbb99f25cfbde
danclark1820 <danclark1820@gmail.com>
2014-04-07 14:13:31 -0400

    Added user associations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,4 +6,6 @@ describe Product do
 
   it { should have_valid(:description).when('Very green, very machine') }
   it { should_not have_valid(:description).when(nil, '') }
+
+  it { should belong_to :user}
 end
