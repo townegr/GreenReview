518f58526fb6fbbddb5937004c47831385711e1c
Greg Towne <townegr@yahoo.com>
2014-04-08 11:26:43 -0400

    product review specs added and passing

--- spec/features/user_creates_review_for_product_spec.rb
+++ spec/features/user_creates_review_for_product_spec.rb
@@ -9,7 +9,7 @@ feature 'authenticated user creates a product review' do
 # I can optionally include a title
 # I must write a description
 
-  scenario 'add product review' do
+  scenario 'add a valid product review' do
     review = FactoryGirl.build(:review)
     prev_count = Review.count
 
@@ -22,5 +22,16 @@ feature 'authenticated user creates a product review' do
 
     expect(Review.count).to eq(prev_count + 1)
     expect(page).to have_content(review.description)
+  end
+
+  scenario 'submit an invalid product review' do
+    user = FactoryGirl.create(:user)
+    prev_count = Review.count
+    sign_in_as(user)
+    visit new_review_path
+    click_on 'Submit Review'
+
+    expect(Review.count).to eq(prev_count)
+    expect(page).to have_content "can't be blank"
   end
 end
