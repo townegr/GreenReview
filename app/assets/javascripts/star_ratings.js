a0ca5bbe9ca98f18ca5c391717446c6c6c53a0e3
danclark1820 <danclark1820@gmail.com>
2014-04-17 15:12:26 -0400

    Refactor to user functions and onclick

--- app/assets/javascripts/star_ratings.js
+++ app/assets/javascripts/star_ratings.js
@@ -1,28 +1,45 @@
 $(function() {
-   $('.rating_star').click(function() {
-     var starClicked = $(this);
-     var stars = starClicked.attr('value');
-     var productId = starClicked.attr('data-chapstick-id');
+// $('.rating_star').click(function() {
+  //   var starClicked = $(this);
+  //   var stars = starClicked.attr('value');
+  //   var productId = starClicked.attr('data-product-id');
 
-     for(i = 1; i <= 5; i++){
-      star = $('#product_'+ productId + '' + i);
-      if (i <= stars){
-        star.AddClass('activated');
-      }else{
-        star.removeClass('activated');
-      }
+  //   for(i = 1; i <= 5; i++) {
+  //     star = $('#product_' + productId + '_' + i);
+  //     if (i <= stars) {
+  //       star.addClass('activated');
+  //     } else {
+  //       star.removeClass('activated');
+  //     }
+  //   }
+});
+
+var rates = function(isNew) {
+  var star = $(window.event.srcElement);
+  var productId = star.attr('data-product-id');
+  var ratingData = {
+    product_id: productId,
+    stars: star.attr('value')
+  };
+
+  if (isNew === "") {
+    requestType = "POST";
+    path = "/ratings";
+  } else {
+    requestType = "PUT";
+    path = "/ratings/" + productId;
+  }
+
+  submitVote(requestType, path, ratingData);
+};
+
+var submitVote = function(requestType, path, data) {
+  $.ajax(
+  {
+    type: requestType,
+    url: path,
+    data: {
+      rating: data
     }
-
-    $.ajax({
-       type: 'POST',
-       url: '/ratings',
-       data: {
-         rating: {
-           // user_id: can't actually pass this in
-           product_id: productId,
-           stars: stars
-         }
-       }
-     });
-   });
- });
+  });
+};
--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,11 +1,29 @@
 class RatingsController < ApplicationController
   def create
-    @rating = Rating.new(rating_params)
+  @rating = Rating.new(rating_params)
+    respond_to do |format|
+      if @rating.save
+        format.json { render json: @rating, status: :created }
+      else
+        puts "ERROR!"
+      end
+    end
+  end
+
+  def update
+    @rating = Rating.find_by(user: current_user, product_id: rating_params[:product_id])
+    respond_to do |format|
+      if @rating.update(rating_params)
+        format.json { render json: @rating, status: :created }
+      else
+        puts "ERROR!"
+      end
+    end
   end
 
   private
   def rating_params
-    params.require(:rating).permit(:user_id, :product_id, :stars)
+    params.require(:rating).permit(:product_id, :stars).merge(user: current_user)
   end
 
 end
